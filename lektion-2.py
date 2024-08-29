0da790c0dc568413c67e402ebdabcbb45877b158
kevzdroolingface <kevin.johhansonelev.ga.ntig.se>
2024-08-29 09:42:52 +0200

    2024-08-29 Vi har gjort if satser

---
+++ lektion-2.py
@@ -0,0 +1,21 @@
+print("Hej och välkommen till mitt program")
+
+namn = input("Vad heter du, skriv ditt namn: ")
+
+print(f"Hej {namn}! Vad kul att du är här!")
+
+print("Hur gammal är du?")
+
+user_age = input("Skriv din ålder i år: ")
+
+print(f"Tack, vad fint att du är {user_age} år ung!")
+user_age_converted = int(user_age)
+
+if user_age_converted > 15:
+    print("Du får köra moppe!")
+
+else:
+
+    print("🤣🤣")
+
+#Lektion nummer två
